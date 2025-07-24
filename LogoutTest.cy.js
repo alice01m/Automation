ba02764ef55d613dc3933735f463d08d205a95a3
Alice M <87013826+alice01m@users.noreply.github.com>
2025-07-24 15:07:54 +0300

    Add files via upload

---
+++ LogoutTest.cy.js
@@ -0,0 +1,24 @@
+describe('Logout functionality', () => {
+
+    it('Logs out successfully', () => {
+
+        // Log in with valid credentials
+        cy.login('standard_user', 'secret_sauce');
+
+        // Check if the user is redirected to the inventory page
+        cy.url().should('include', '/inventory.html');
+
+        // Open the sidebar menu
+        cy.get('#react-burger-menu-btn').click();
+
+        // Click the Logout button in the side menu
+        cy.get('[data-test="logout-sidebar-link"]').click();
+
+        // Check if the login form is visible again
+        cy.get('#login_button_container').should('be.visible');
+
+        // Check if the user is redirected to the login page
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+
+    })
+})
